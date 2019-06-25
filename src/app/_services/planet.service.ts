5a97938542701c81460121743a0074a29c3bb34e
Carlos Magioli <carlos.magioli@celeritech.com.br>
2019-06-24 22:51:53 -0300

    Add get all planets method

--- src/app/_services/planet.service.ts
+++ src/app/_services/planet.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Planet } from '../_model/planet';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlanetService {
 
-  constructor() { }
+  public url = environment.core_api + 'planets'; // URL to web api
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Planet[]> {
+      return this.http.get<Planet[]>(this.url);
+  }
+
 }
