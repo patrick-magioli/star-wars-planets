ce63f95c3f7b496f50e1e8b44161b593a45fba0c
Carlos Magioli <carlos.magioli@celeritech.com.br>
2019-06-24 22:28:03 -0300

    Add model and planet class

---
+++ src/app/_model/planet.ts
@@ -0,0 +1,16 @@
+export class Planet {
+    name: string;
+    rotationPeriod: number;
+    orbitalPeriod: number;
+    diameter: number;
+    climate: string;
+    gravity: string;
+    terrain: string;
+    surfaceWater: number;
+    population: number;
+    residents: [];
+    films: [];
+    created: Date;
+    edited: Date;
+    url: string;
+}
