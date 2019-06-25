ce5bc91947514de8d6c9f88c2d8afdb53ddfc454
Carlos Magioli <carlos.magioli@celeritech.com.br>
2019-06-24 23:11:25 -0300

    Update planet service for include page main

--- src/app/_services/planet.service.ts
+++ src/app/_services/planet.service.ts
@@ -2,19 +2,19 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Planet } from '../_model/planet';
+import { Page } from '../_model/page';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlanetService {
 
-  public url = environment.core_api + 'planets'; // URL to web api
+  public url = environment.core_api + 'planets/?format=json'; // URL to web api
 
   constructor(private http: HttpClient) { }
 
-  getAll(): Observable<Planet[]> {
-      return this.http.get<Planet[]>(this.url);
+  getAll(): Observable<Page[]> {
+      return this.http.get<Page[]>(this.url);
   }
 
 }
