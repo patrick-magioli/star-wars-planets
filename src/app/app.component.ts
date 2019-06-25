8cc0954c5b4147888de05d16e834e2e318f69087
Carlos Magioli <carlos.magioli@celeritech.com.br>
2019-06-24 22:52:24 -0300

    Add test service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlanetService } from './_services/planet.service';
+import { Planet } from './_model/planet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'star-wars-planets';
+export class AppComponent implements OnInit {
+
+  public planets: Planet[];
+
+  constructor(private planetService: PlanetService) { }
+
+  ngOnInit() {
+    this.planetService.getAll().subscribe(
+      (data: Planet[]) => {
+        this.planets = data;
+        console.log(this.planets);
+      }
+    );
+  }
 }
