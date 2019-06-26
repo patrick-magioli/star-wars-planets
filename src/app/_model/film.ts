204a1c890f739e4f68245ecd79df0d5e7a01707a
Carlos Magioli <carlos.magioli@celeritech.com.br>
2019-06-26 00:37:06 -0300

    add film object class

---
+++ src/app/_model/film.ts
@@ -0,0 +1,16 @@
+export class Film {
+    title: string;
+    episodeId: number;
+    openingCrawl: string;
+    director: string;
+    producer: string;
+    releaseDate: Date;
+    characters: [];
+    planets: [];
+    starships: [];
+    vehicles: [];
+    species: [];
+    created: Date;
+    edited: Date;
+    url: Date;
+}
